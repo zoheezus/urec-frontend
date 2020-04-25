bb3ce03de3694a4fa925a5cba61166495d726baa
zoheezus <zohaib0126@gmail.com>
2020-04-25 14:30:58 -0400

    Updates

---
+++ precache-manifest.87470232295c4c5ad9361153f6befc8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf05772d09ed05ff31ba9c92bff347dd",
+    "url": "/urec-frontend/index.html"
+  },
+  {
+    "revision": "61ef0ad738b23a570441",
+    "url": "/urec-frontend/static/css/main.bcbfbd77.chunk.css"
+  },
+  {
+    "revision": "507b3784655cddfcde94",
+    "url": "/urec-frontend/static/js/2.997129aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/urec-frontend/static/js/2.997129aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ef0ad738b23a570441",
+    "url": "/urec-frontend/static/js/main.25f467d9.chunk.js"
+  },
+  {
+    "revision": "8cfaab08f4aae2c4b156",
+    "url": "/urec-frontend/static/js/runtime-main.79d628b6.js"
+  }
+]);
